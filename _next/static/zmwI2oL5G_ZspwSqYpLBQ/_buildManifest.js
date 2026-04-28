6a944304a162901d31da1971cb18c955f2c618b9
NORT <nort1fit@gmail.com>
2026-04-29 06:07:12 +0700

    Deploy NORT fat loss reset app

---
+++ _next/static/zmwI2oL5G_ZspwSqYpLBQ/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/nort-fat-loss-reset//_next/:path+",
+        "destination": "/nort-fat-loss-reset/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
